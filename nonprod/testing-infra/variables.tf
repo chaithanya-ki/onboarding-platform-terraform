f8f44d1077507f3b9440fdefc17eecde34ee91ee
chaithanya-ki <138691616+chaithanya-ki@users.noreply.github.com>
2023-10-01 21:36:15 -0500

    Update variables.tf

--- nonprod/testing-infra/variables.tf
+++ nonprod/testing-infra/variables.tf
@@ -1,8 +1,8 @@
 variable "resource_group_name" {
   description = "Name of the Azure resource group"
   type        = string
-  default     = "my-resource-group2"
-}
+  default     = "my-resource-group"
+}        
 
 variable "resource_group_location" {
   description = "Location for the Azure resource group"
